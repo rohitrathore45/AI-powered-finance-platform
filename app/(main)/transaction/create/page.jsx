d58e2f0634793c22ad993b5aefe95a957dba9410
Rohit Rathore <rohitjeevanrathore@gmail.com>
2025-06-28 11:33:04 +0530

    full-project

--- app/(main)/transaction/create/page.jsx
+++ app/(main)/transaction/create/page.jsx
@@ -1,32 +1,32 @@
-import { getUserAccounts } from '@/actions/dashboard'
+import { getUserAccounts } from '@/actions/dashboard';
 import { defaultCategories } from '@/data/categories';
-import React from 'react'
+import React from 'react';
 import AddTransactionForm from '../_components/transactionForm';
 import { getTransaction } from '@/actions/transaction';
 
-const AddTransactionPage = async ({ searchParams }) => {
-    const accounts = await getUserAccounts();
+export default async function AddTransactionPage({ searchParams }) {
+  const accounts = await getUserAccounts();
 
-    const editId = searchParams?.edit;
+  const editId = searchParams?.edit;
 
-    let initialData = null;
-    if(editId){
-      const transaction = await getTransaction(editId);
-      initialData = transaction;
-    }
+  let initialData = null;
+  if (editId) {
+    const transaction = await getTransaction(editId);
+    initialData = transaction;
+  }
 
   return (
     <div className='max-w-3xl mx-auto px-5'>
-        <h1 className='text-5xl gradient-title mb-8'>{ editId ? "Edit" : "Add" } Transaction</h1>
+      <h1 className='text-5xl gradient-title mb-8'>
+        {editId ? 'Edit' : 'Add'} Transaction
+      </h1>
 
-        <AddTransactionForm 
-          accounts={accounts} 
-          categories={defaultCategories}
-          editMode={!!editId}
-          initialData={initialData}
-        />
+      <AddTransactionForm 
+        accounts={accounts} 
+        categories={defaultCategories}
+        editMode={!!editId}
+        initialData={initialData}
+      />
     </div>
-  )
+  );
 }
-
-export default AddTransactionPage
